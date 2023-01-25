011b9ab2e40b8828c950486860cd8ce9639666ae
Alex Braun <alexander.g.braun@gmail.com>
2023-01-25 07:22:17 +0000

    jaccards_loss

--- python/flatiron/core/loss.py
+++ python/flatiron/core/loss.py
@@ -1,9 +1,15 @@
+from typing import Union
+import numpy
+import tensorflow as tf
+
 import tensorflow.keras.backend as tfb
+
+Arraylike = Union[numpy.ndarray, tf.Tensor]
 # ------------------------------------------------------------------------------
 
 
 def jaccards_loss(y_true, y_pred, smooth=100):
-    # type: ()
+    # type: (Arraylike, Arraylike, int) -> tf.Tensor
     '''
     Jaccard's loss is usefull for unbalanced datasets. This has been shifted so
     it converges on 0 and is smoothed to avoid exploding or disappearing
@@ -14,12 +20,12 @@ def jaccards_loss(y_true, y_pred, smooth=100):
     See: https://en.wikipedia.org/wiki/Jaccard_index
 
     Args:
-        y_true (): Ground truth labels.
-        y_pred (): Predicted labels.
-        smooth (int, optional): Smoothing factor.
+        y_true (NDArray or Tensor): Ground truth labels.
+        y_pred (NDArray or Tensor): Predicted labels.
+        smooth (int, optional): Smoothing factor. Default: 100.
 
     Returns:
-
+        Tensor: Loss function.
     '''
     intersection = tfb.sum(tfb.abs(y_true * y_pred), axis=-1)
     union = tfb.sum(tfb.abs(y_true) + tfb.abs(y_pred), axis=-1)
