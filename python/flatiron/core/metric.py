2d73283af4bd69ba0983fc8b16333682e88882a5
Alex Braun <alexander.g.braun@gmail.com>
2023-01-27 07:20:49 +0000

    added dice

--- python/flatiron/core/metric.py
+++ python/flatiron/core/metric.py
@@ -121,3 +121,64 @@ def jaccard(y_true, y_pred):
     jacc = (i + 1.0) / (u + 1.0)
     jacc = tfkb.mean(jacc)
     return jacc
+
+
+def dice(y_true, y_pred, smooth=1.0):
+    # type: (tf.Tensor, tf.Tensor, float) -> tf.Tensor
+    r'''
+    Dice metric.
+
+    Equation:
+
+    .. math::
+        :nowrap:
+
+            \begin{alignat*}{3}
+                \definecolor{blue2}{rgb}{0.58, 0.71, 0.9}
+                \definecolor{cyan2}{rgb}{0.71, 0.93, 0.95}
+                \definecolor{green2}{rgb}{0.63, 0.82, 0.48}
+                \definecolor{light1}{rgb}{0.64, 0.64, 0.64}
+                \definecolor{red2}{rgb}{0.87, 0.58, 0.56}
+
+                \color{cyan2} Dice(y, \hat{y}) && = \frac{2 * I + S}{U_s + S}
+            \end{alignat*}
+
+    Terms:
+
+    .. math::
+        :nowrap:
+
+            \begin{alignat*}{3}
+                intersection & \rightarrow \color{red2} 
+                    I(y, \hat{y}) && = \sum{(y_i * \hat{y_i})} 
+                \\
+                \text{simple union} & \rightarrow \color{green2} 
+                    U_s(y, \hat{y}) && = \sum{(y_i + \hat{y_i})}
+                \\
+                \text{smoothing factor} & \rightarrow \color{blue2} S 
+                \\ 
+                \text{expansion} & \rightarrow 
+                    \color{cyan2} Dice(y, \hat{y}, S) && = 
+                    \frac{ 
+                        \color{white} 2 * \color{red2} \sum{(y_i * \hat{y_i})} 
+                        \color{white} + \color{blue2} S
+                    }{
+                        \color{green2} \sum{(y_i + \hat{y_i})}
+                        \color{white} + \color{blue2} S
+                    } 
+            \end{alignat*}
+
+    Args:
+        y_true (tf.Tensor): True labels.
+        y_pred (tf.Tensor): Predicted labels.
+        smooth (float, optional): Smoothing factor. Default: 1.0
+
+    Returns:
+        tf.Tensor: Dice metric.
+    '''
+    yt = tfkb.flatten(y_true)
+    yp = tfkb.flatten(y_pred)
+    i = tfkb.sum(yt * yp)
+    u = tfkb.sum(yt) + tfkb.sum(yp)
+    dice = (2.0 * i + smooth) / (u + smooth)
+    return dice
