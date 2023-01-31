aef655dae75e8340f7ab0c7a7b6592df47e48d6f
Alex Braun <alexander.g.braun@gmail.com>
2023-01-31 06:41:48 +0000

    DatasetConfig

--- python/flatiron/core/__init__.py
+++ python/flatiron/core/__init__.py
@@ -1,4 +1,5 @@
 import flatiron.core.dataset  # noqa F401
+import flatiron.core.dataset_config  # noqa F401
 import flatiron.core.logging  # noqa F401
 import flatiron.core.tools  # noqa F401
 import flatiron.core.validators  # noqa F401
