bdb15219822e8dded9fc3bde00205267ddafb2cc
Alex Braun <alexander.g.braun@gmail.com>
2023-01-31 10:36:26 +0000

    WIP

--- python/flatiron/core/pipeline.py
+++ python/flatiron/core/pipeline.py
@@ -1,22 +1,23 @@
 from typing import Union  # noqa F401
 import schematics.models as scm  # noqa F401
 
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
 from copy import deepcopy
 from pathlib import Path
+import math
 
 import yaml
+import tensorflow.keras.optimizer as tfko
 
 from flatiron.core.dataset import Dataset
 from flatiron.core.dataset_config import DatasetConfig
+import flatiron.core.tools as fict
 
 Filepath = Union[str, Path]
 # ------------------------------------------------------------------------------
 
 
 class PipelineBase(ABC):
-    _model_config_class = None
-
     @classmethod
     def read_yaml(cls, filepath):
         # type: (Filepath) -> PipelineBase
@@ -59,8 +60,8 @@ class PipelineBase(ABC):
         '''
         config = deepcopy(config)
         config['dataset'] = self._validate(config['dataset'], DatasetConfig)
-        config['model'] = self._validate(config['model'], self._model_config_class)
-        config['compile'] = self._validate(config['compile'], CompileConfig)
+        config['model'] = self._validate(config['model'], self.model_config)
+        # config['train'] = self._validate(config['train'], TrainConfig)
         self.config = config
 
         src = config['dataset']['source']
@@ -97,6 +98,15 @@ class PipelineBase(ABC):
         self.dataset.unload()
         return self
 
+    @abstractproperty
+    def model_config(self):
+        # type: () -> scm.Model
+        '''
+        Returns:
+            scm.Model: Model config class.
+        '''
+        pass
+
     @abstractmethod
     def build(self):
         # type: () -> PipelineBase
@@ -110,5 +120,34 @@ class PipelineBase(ABC):
 
     def compile(self):
         config = self.config['compile']
-        self.model.compile(**config)
+        opt = tfko.get(config['optimizer'], **config['optimizer_params'])
+        self.model.compile(
+            optimizer=opt,
+            loss=loss,
+            metrics=metrics,
+        )
+        return self
+
+    def fit(self):
+        config = self.config['fit']
+        temp = fict.get_tensorboard_project(
+            config['project'],
+            config['root'],
+            config['timezone'],
+        )
+        callbacks = fict.get_callbacks(
+            temp['log_directory'],
+            temp['checkpoint_pattern'],
+            config['checkpoint_params'],
+        )
+        self.model.fit(
+            metric=config['metric'],
+            mode=config['mode'],
+            save_best_only=config['save_best_only'],
+            save_freq=config['save_freq'],
+            update_freq=config['update_freq'],
+            steps_per_epoch=math.ceil(self.x_train.shape[0] / config['batch_size']),
+            validation_data=(self.x_test, self.y_test),
+            callbacks=callbacks,
+        )
         return self
