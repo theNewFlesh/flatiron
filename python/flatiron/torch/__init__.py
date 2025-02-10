1ff98a13471070a4fb2e46cc57f6b1a8f38749cb
Alex Braun <alexander.g.braun@gmail.com>
2025-02-10 23:20:27 +0000

    metric

--- python/flatiron/torch/__init__.py
+++ python/flatiron/torch/__init__.py
@@ -1,5 +1,5 @@
 import flatiron.torch.loss  # noqa F401
-# import flatiron.torch.metric  # noqa F401
+import flatiron.torch.metric  # noqa F401
 # import flatiron.torch.models  # noqa F401
 import flatiron.torch.optimizer  # noqa F401
 # import flatiron.torch.tools  # noqa F401
---
+++ python/flatiron/torch/metric.py
@@ -0,0 +1,23 @@
+from torch.nn import Module  # noqa: F401
+
+import torchmetrics as torchmetric
+
+import flatiron.core.tools as fict
+# ------------------------------------------------------------------------------
+
+
+def get(name):
+    # type: (str) -> Module
+    '''
+    Get function from this module.
+
+    Args:
+        name (str): Function name.
+
+    Returns:
+        function: Module function.
+    '''
+    try:
+        return fict.get_module_class(name, __name__)
+    except NotImplementedError:
+        return getattr(torchmetric, name)
