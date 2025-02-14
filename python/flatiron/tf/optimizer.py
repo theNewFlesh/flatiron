84a302c2cd7f1aa05d1bffc69df393cb0b267160
Alex Braun <alexander.g.braun@gmail.com>
2025-02-14 19:19:56 +0000

    tf.opt.get

--- python/flatiron/tf/optimizer.py
+++ python/flatiron/tf/optimizer.py
@@ -7,18 +7,18 @@ import flatiron.core.tools as fict
 # ------------------------------------------------------------------------------
 
 
-def get(params):
-    # type: (dict) -> Callable[[Any], Any]
+def get(name):
+    # type: (str) -> Callable[[Any], Any]
     '''
     Get function from this module.
 
     Args:
-        params (dict): Optimizer params.
+        name (str): Optimizer name.
 
     Returns:
         function: Module function.
     '''
     try:
-        return fict.get_module_function(params['class_name'], __name__)
+        return fict.get_module_function(name, __name__)
     except NotImplementedError:
-        return tfoptim.get(params)
+        return tfoptim.get(dict(class_name=name))
--- python/flatiron/tf/optimizer_test.py
+++ python/flatiron/tf/optimizer_test.py
@@ -6,5 +6,5 @@ import flatiron.tf.optimizer as fi_tfoptim
 
 class TFOptimizerTests(unittest.TestCase):
     def test_get(self):
-        fi_tfoptim.get(dict(class_name='sgd'))
-        fi_tfoptim.get(dict(class_name='adam'))
+        fi_tfoptim.get('sgd')
+        fi_tfoptim.get('adam')
