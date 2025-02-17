6d7f3df8bffc4436e2ad0ca23f6204dc1908cce3
Alex Braun <alexander.g.braun@gmail.com>
2025-02-17 21:05:46 +0000

    batch=1

--- python/flatiron/tf/tools.py
+++ python/flatiron/tf/tools.py
@@ -34,7 +34,7 @@ def get_callbacks(log_directory, checkpoint_pattern, checkpoint_params={}):
     fict.enforce_callbacks(log_directory, checkpoint_pattern)
     return dict(
         tensorboard=tfcallbacks.TensorBoard(
-            log_dir=log_directory, histogram_freq=1, update_freq='batch'
+            log_dir=log_directory, histogram_freq=1, update_freq=1
         ),
         checkpoint=tfcallbacks.ModelCheckpoint(checkpoint_pattern, **checkpoint_params),
     )
