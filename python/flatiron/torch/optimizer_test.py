025b7a2511876fafbcad667709fcc33c9ed3cdda
Alex Braun <alexander.g.braun@gmail.com>
2025-02-21 01:55:18 +0000

    DummyModel

--- python/flatiron/torch/optimizer_test.py
+++ python/flatiron/torch/optimizer_test.py
@@ -1,12 +1,12 @@
 import unittest
 
 import flatiron.torch.optimizer as fi_torchoptim
-from flatiron.torch.tools_test import SimpleModel
+from flatiron.torch.tools_test import DummyModel
 # ------------------------------------------------------------------------------
 
 
 class TorchOptimizerTests(unittest.TestCase):
     def test_get(self):
-        model = SimpleModel(2, 2)
+        model = DummyModel(2, 2)
         fi_torchoptim.get(dict(name='sgd', learning_rate=0.01), model)
         fi_torchoptim.get(dict(name='adam'), model)
