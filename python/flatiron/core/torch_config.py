54550e28446837fc280a4c22a70f8500331d8d9f
Alex Braun <alexander.g.braun@gmail.com>
2025-02-22 18:53:48 +0000

    torch loss

--- python/flatiron/core/torch_config.py
+++ python/flatiron/core/torch_config.py
@@ -4,11 +4,12 @@ import pydantic as pyd
 # ------------------------------------------------------------------------------
 
 
+# BASE--------------------------------------------------------------------------
 class TorchBaseConfig(pyd.BaseModel):
     name: str
 
 
-# OPTIMIZER---------------------------------------------------------------------
+# OPTIMIZER HELPERS
 class TorchOptBaseConfig(TorchBaseConfig):
     learning_rate: float = 0.01  # convert to lr
 
@@ -42,18 +43,43 @@ class TBeta(pyd.BaseModel):
     beta_2: float = 0.999  # convert to betas: tuple[float, float]
 
 
-class TGroup(TCap, TDecay, TDiff, TEps, TFor, TMax):
+class TGroup1(TCap, TDecay, TDiff, TEps, TFor, TMax):
     pass
-# ------------------------------------------------------------------------------
 
 
+# LOSS HELPERS
+class TReduct(pyd.BaseModel):
+    reduction: str = 'mean'
+
+
+class TRed(pyd.BaseModel):
+    reduce: OptBool = None
+
+
+class TSize(pyd.BaseModel):
+    size_average: OptBool = None
+
+
+class TMarg(pyd.BaseModel):
+    margin: float = 0.0
+
+
+class TGroup2(TRed, TReduct, TSize):
+    pass
+
+
+class TGroup3(TMarg, TRed, TReduct, TSize):
+    pass
+
+
+# OPTIMIZER---------------------------------------------------------------------
 class TorchOptASGDConfig(TorchOptBaseConfig, TCap, TDecay, TDiff, TFor, TMax):
     alpha: float = 0.75
     lambd: float = 0.0001
     t0: float = 1000000.0
 
 
-class TorchOptAdadeltaConfig(TorchOptBaseConfig, TGroup):
+class TorchOptAdadeltaConfig(TorchOptBaseConfig, TGroup1):
     rho: float = 0.9
 
 
@@ -68,17 +94,17 @@ class TorchOptAdagradConfig(TorchOptBaseConfig, TDecay, TDiff, TEps, TFor, TMax)
     lr_decay: float = 0
 
 
-class TorchOptAdamConfig(TorchOptBaseConfig, TGroup, TBeta):
+class TorchOptAdamConfig(TorchOptBaseConfig, TGroup1, TBeta):
     amsgrad: bool = False
     fused: OptBool = None
 
 
-class TorchOptAdamWConfig(TorchOptBaseConfig, TGroup, TBeta):
+class TorchOptAdamWConfig(TorchOptBaseConfig, TGroup1, TBeta):
     amsgrad: bool = False
     fused: OptBool = None
 
 
-class TorchOptAdamaxConfig(TorchOptBaseConfig, TGroup, TBeta):
+class TorchOptAdamaxConfig(TorchOptBaseConfig, TGroup1, TBeta):
     pass
 
 
@@ -91,15 +117,15 @@ class TorchOptLBFGSConfig(TorchOptBaseConfig):
     tolerance_grad: float = 1e-07
 
 
-class TorchOptNAdamConfig(TorchOptBaseConfig, TGroup, TBeta):
+class TorchOptNAdamConfig(TorchOptBaseConfig, TGroup1, TBeta):
     momentum_decay: float = 0.004
 
 
-class TorchOptRAdamConfig(TorchOptBaseConfig, TGroup, TBeta):
+class TorchOptRAdamConfig(TorchOptBaseConfig, TGroup1, TBeta):
     pass
 
 
-class TorchOptRMSpropConfig(TorchOptBaseConfig, TGroup):
+class TorchOptRMSpropConfig(TorchOptBaseConfig, TGroup1):
     alpha: float = 0.99
     centered: bool = False
     momentum: float = 0
@@ -119,3 +145,97 @@ class TorchOptSGDConfig(TorchOptBaseConfig, TDecay, TDiff, TFor, TMax):
 
 class TorchOptSparseAdamConfig(TorchOptBaseConfig, TEps, TMax, TBeta):
     pass
+
+
+# LOSS--------------------------------------------------------------------------
+class TorchBCELossConfig(TorchBaseConfig, TGroup2):
+    pass
+
+
+class TorchBCEWithLogitsLossConfig(TorchBaseConfig, TGroup2):
+    pass
+
+
+class TorchCTCLossConfig(TorchBaseConfig, TReduct):
+    blank: int = 0
+    zero_infinity: bool = False
+
+
+class TorchCosineEmbeddingLossConfig(TorchBaseConfig, TGroup3):
+    pass
+
+
+class TorchCrossEntropyLossConfig(TorchBaseConfig, TGroup2):
+    ignore_index: int = -100
+    label_smoothing: float = 0.0
+
+
+class TorchGaussianNLLLossConfig(TorchBaseConfig, TEps, TReduct):
+    full: bool = False
+
+
+class TorchHingeEmbeddingLossConfig(TorchBaseConfig, TGroup3):
+    pass
+
+
+class TorchHuberLossConfig(TorchBaseConfig, TReduct):
+    delta: float = 1.0
+
+
+class TorchKLDivLossConfig(TorchBaseConfig, TGroup2):
+    log_target: bool = False
+
+
+class TorchL1LossConfig(TorchBaseConfig, TGroup2):
+    pass
+
+
+class TorchMSELossConfig(TorchBaseConfig, TGroup2):
+    pass
+
+
+class TorchMarginRankingLossConfig(TorchBaseConfig, TGroup3):
+    pass
+
+
+class TorchMultiLabelMarginLossConfig(TorchBaseConfig, TGroup2):
+    pass
+
+
+class TorchMultiLabelSoftMarginLossConfig(TorchBaseConfig, TGroup2):
+    pass
+
+
+class TorchMultiMarginLossConfig(TorchBaseConfig, TGroup3):
+    exponent: int = 1  # convert to p
+
+
+class TorchNLLLossConfig(TorchBaseConfig, TGroup2):
+    ignore_index: int = -100
+
+
+class TorchPairwiseDistanceConfig(TorchBaseConfig, TEps):
+    keepdim: bool = False
+    norm_degree: float = 2.0  # convert to p
+
+
+class TorchPoissonNLLLossConfig(TorchBaseConfig, TEps, TGroup2):
+    full: bool = False
+    log_input: bool = True
+
+
+class TorchSmoothL1LossConfig(TorchBaseConfig, TGroup2):
+    beta: float = 1.0
+
+
+class TorchSoftMarginLossConfig(TorchBaseConfig, TGroup2):
+    pass
+
+
+class TorchTripletMarginLossConfig(TorchBaseConfig, TEps, TGroup3):
+    norm_degree: float = 2.0  # convert to p
+    swap: bool = False
+
+
+class TorchTripletMarginWithDistanceLossConfig(TorchBaseConfig, TMarg, TReduct):
+    swap: bool = False
