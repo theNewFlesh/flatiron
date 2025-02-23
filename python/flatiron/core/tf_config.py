211308ab6a9c355afbfc9215f46558047bc22a22
Alex Braun <alexander.g.braun@gmail.com>
2025-02-23 16:42:16 +0000

    tf_config metrics

--- python/flatiron/core/tf_config.py
+++ python/flatiron/core/tf_config.py
@@ -1,3 +1,4 @@
+from typing import Optional, Union
 from flatiron.core.types import OptInt, OptFloat, OptStr
 
 import pydantic as pyd
@@ -8,7 +9,7 @@ class TFBaseConfig(pyd.BaseModel):
     name: str
 
 
-# OPTIMIZER---------------------------------------------------------------------
+# OPTIMIZER-HELPERS-------------------------------------------------------------
 class TFOptBaseConfig(TFBaseConfig):
     clipnorm: OptFloat = None
     clipvalue: OptFloat = None
@@ -29,9 +30,40 @@ class TFEpsilon(pyd.BaseModel):
 class TFBeta(pyd.BaseModel):
     beta_1: float = 0.9
     beta_2: float = 0.99
-# ------------------------------------------------------------------------------
 
 
+# LOSS-HELPERS------------------------------------------------------------------
+class TFLossBaseConfig(TFBaseConfig):
+    dtype: OptStr = None
+    reduction: str = 'sum_over_batch_size'
+
+
+class TFAxis(pyd.BaseModel):
+    axis: int = -1
+
+
+class TFLogits(pyd.BaseModel):
+    from_logits: bool = False
+
+
+# METRIC-HELPERS----------------------------------------------------------------
+class TFMetricBaseConfig(TFBaseConfig):
+    dtype: OptStr = None
+
+
+class TFThresh(pyd.BaseModel):
+    thresholds: Optional[Union[float, list[float]]] = None
+
+
+class TFClsId(pyd.BaseModel):
+    class_id: OptInt = None
+
+
+class TFNumThresh(pyd.BaseModel):
+    num_thresholds: int = 200
+
+
+# OPTIMIZERS--------------------------------------------------------------------
 class TFOptAdafactorConfig(TFOptBaseConfig):
     beta_2_decay: float = -0.8
     clip_threshold: float = 1.0
@@ -92,21 +124,7 @@ class TFOptRMSpropConfig(TFOptBaseConfig, TFEpsilon):
     rho: float = 0.9
 
 
-# LOSS--------------------------------------------------------------------------
-class TFLossBaseConfig(TFBaseConfig):
-    dtype: OptStr = None
-    reduction: str = 'sum_over_batch_size'
-
-
-class TFAxis(pyd.BaseModel):
-    axis: int = -1
-
-
-class TFLogits(pyd.BaseModel):
-    from_logits: bool = False
-# ------------------------------------------------------------------------------
-
-
+# LOSSES------------------------------------------------------------------------
 class TFLossBinaryCrossentropyConfig(TFLossBaseConfig, TFAxis, TFLogits):
     label_smoothing: float = 0.0
 
@@ -153,3 +171,209 @@ class TFLossSparseCategoricalCrossentropyConfig(TFLossBaseConfig, TFLogits):
 class TFLossTverskyConfig(TFLossBaseConfig, TFAxis):
     alpha: float = 0.5
     beta: float = 0.5
+
+
+# METRICS-----------------------------------------------------------------------
+class TFMetricAUC(TFMetricBaseConfig, TFNumThresh, TFThresh):
+    curve: str = 'ROC'
+    from_logits: bool = False
+    label_weights: Optional[list[float]] = None
+    multi_label: bool = False
+    num_labels: OptInt = None
+    summation_method: str = 'interpolation'
+
+
+class TFMetricAccuracy(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricBinaryAccuracy(TFMetricBaseConfig):
+    threshold: float = 0.5
+
+
+class TFMetricBinaryCrossentropy(TFMetricBaseConfig):
+    from_logits: bool = False
+    label_smoothing: int = 0
+
+
+class TFMetricBinaryIoU(TFMetricBaseConfig):
+    target_class_ids: list[int] = [0, 1]
+    threshold: float = 0.5
+
+
+class TFMetricCategoricalAccuracy(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricCategoricalCrossentropy(TFMetricBaseConfig, TFAxis):
+    from_logits: bool = False
+    label_smoothing: int = 0
+
+
+class TFMetricCategoricalHinge(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricConcordanceCorrelation(TFMetricBaseConfig, TFAxis):
+    pass
+
+
+class TFMetricCosineSimilarity(TFMetricBaseConfig, TFAxis):
+    pass
+
+
+class TFMetricF1Score(TFMetricBaseConfig):
+    average: OptStr = None
+    threshold: OptFloat = None
+
+
+class TFMetricFBetaScore(TFMetricBaseConfig):
+    average: OptStr = None
+    beta: float = 1.0
+    threshold: OptFloat = None
+
+
+class TFMetricFalseNegatives(TFMetricBaseConfig, TFThresh):
+    pass
+
+
+class TFMetricFalsePositives(TFMetricBaseConfig, TFThresh):
+    pass
+
+
+class TFMetricHinge(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricIoU(TFMetricBaseConfig, TFAxis):
+    ignore_class: OptInt = None
+    num_classes: int
+    sparse_y_pred: bool = True
+    sparse_y_true: bool = True
+    target_class_ids: list[int]
+
+
+class TFMetricKLDivergence(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricLogCoshError(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricMean(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricMeanAbsoluteError(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricMeanAbsolutePercentageError(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricMeanIoU(TFMetricBaseConfig, TFAxis):
+    ignore_class: OptInt = None
+    num_classes: int
+    sparse_y_pred: bool = True
+    sparse_y_true: bool = True
+
+
+class TFMetricMeanSquaredError(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricMeanSquaredLogarithmicError(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricMetric(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricOneHotIoU(TFMetricBaseConfig, TFAxis):
+    ignore_class: OptInt = None
+    num_classes: int
+    sparse_y_pred: bool = False
+    target_class_ids: list[int]
+
+
+class TFMetricOneHotMeanIoU(TFMetricBaseConfig, TFAxis):
+    ignore_class: OptInt = None
+    num_classes: int
+    sparse_y_pred: bool = False
+
+
+class TFMetricPearsonCorrelation(TFMetricBaseConfig, TFAxis):
+    pass
+
+
+class TFMetricPoisson(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricPrecision(TFMetricBaseConfig, TFClsId, TFThresh):
+    top_k: OptInt = None
+
+
+class TFMetricPrecisionAtRecall(TFMetricBaseConfig, TFClsId, TFNumThresh):
+    recall: float
+
+
+class TFMetricR2Score(TFMetricBaseConfig):
+    class_aggregation: str = 'uniform_average'
+    num_regressors: int = 0
+
+
+class TFMetricRecall(TFMetricBaseConfig, TFClsId, TFThresh):
+    top_k: OptInt = None
+
+
+class TFMetricRecallAtPrecision(TFMetricBaseConfig, TFClsId, TFNumThresh):
+    precision: float
+
+
+class TFMetricRootMeanSquaredError(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricSensitivityAtSpecificity(TFMetricBaseConfig, TFClsId, TFNumThresh):
+    specificity: float
+
+
+class TFMetricSparseCategoricalAccuracy(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricSparseCategoricalCrossentropy(TFMetricBaseConfig, TFAxis):
+    from_logits: bool = False
+
+
+class TFMetricSparseTopKCategoricalAccuracy(TFMetricBaseConfig):
+    from_sorted_ids: bool = False
+    k: int = 5
+
+
+class TFMetricSpecificityAtSensitivity(TFMetricBaseConfig, TFClsId, TFNumThresh):
+    sensitivity: float
+
+
+class TFMetricSquaredHinge(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricSum(TFMetricBaseConfig):
+    pass
+
+
+class TFMetricTopKCategoricalAccuracy(TFMetricBaseConfig):
+    k: int = 5
+
+
+class TFMetricTrueNegatives(TFMetricBaseConfig, TFThresh):
+    pass
+
+
+class TFMetricTruePositives(TFMetricBaseConfig, TFThresh):
+    pass
