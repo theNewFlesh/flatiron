c2a019d951b746b9deeb1c1a0d93de24e2e27053
Alex Braun <alexander.g.braun@gmail.com>
2025-03-09 04:21:08 +0000

    rst

--- python/flatiron/core/__init__.py
+++ python/flatiron/core/__init__.py
@@ -3,6 +3,7 @@ import flatiron.core.dataset  # noqa F401
 import flatiron.core.logging  # noqa F401
 import flatiron.core.multidataset  # noqa F401
 import flatiron.core.pipeline  # noqa F401
+import flatiron.core.resolve  # noqa F401
 import flatiron.core.tools  # noqa F401
 import flatiron.core.types  # noqa F401
 import flatiron.core.validators  # noqa F401
