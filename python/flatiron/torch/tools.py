504fe71c3ef7bb7920b83748e794a24cdd68e910
Alex Braun <alexander.g.braun@gmail.com>
2025-03-10 15:44:12 +0000

    lint

--- python/flatiron/torch/tools.py
+++ python/flatiron/torch/tools.py
@@ -184,7 +184,7 @@ class TorchDataset(Dataset, torchdata.Dataset):
         this._ext_regex = dataset._ext_regex
         this._calc_file_size = dataset._calc_file_size
         this._sample_gb = dataset._sample_gb
-        this._channels_first = channels_first  # type: ignore
+        this._channels_first = channels_first
         return this
 
     def __getitem__(self, frame):
@@ -201,7 +201,7 @@ class TorchDataset(Dataset, torchdata.Dataset):
         items = [x.copy() for x in items]
 
         # pytorch expects (C, H, W) because it sucks
-        if self._channels_first:  # type: ignore
+        if self._channels_first:
             arrays = items
             items = []
             for item in arrays:
